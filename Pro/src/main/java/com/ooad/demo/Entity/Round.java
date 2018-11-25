12286786e8ba352ddb82be0ae23116e9cc404ea1
17Wang <1520166121@qq.com>
2018-11-25 15:48:33 +0800

    设置Round和Seminar的实体类

--- Pro/src/main/java/com/ooad/demo/Entity/Round.java
+++ Pro/src/main/java/com/ooad/demo/Entity/Round.java
@@ -4,6 +4,7 @@ public class Round {
     //基础信息
     private int id;
     private int courseId;           //所属课程id
+    private int order;               //轮次
     private int calculatePreType;
     private int calculateQueType;
     private int calculateRepType;
--- Pro/src/main/java/com/ooad/demo/Entity/Seminar.java
+++ Pro/src/main/java/com/ooad/demo/Entity/Seminar.java
@@ -1,9 +1,22 @@
 package com.ooad.demo.Entity;
 
+import java.sql.Timestamp;
+
 public class Seminar {
+    //基本信息
     private int id;
     private int roundId;           //属于哪个round 默认为0 不属于任何一个
     private String topic;           //讨论课主题
+    private String intro;            //讨论课介绍
     private boolean canVisible;   //是否可见
     private boolean isShare;       //是否共享
+    private Timestamp startTime;    //讨论课开始时间
+    private boolean isFinished;    //是否结束
+    private boolean isStarted;     //是否开始
+
+    //规则
+    private int teamNumLimit;
+    private Timestamp reportDDL;
+    private Timestamp signUpStartTime;
+    private Timestamp signUpEndTime;
 }
