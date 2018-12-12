40b677244a1c38d8d7849c285843511ff17880e2
17Wang <1520166121@qq.com>
2018-12-12 14:53:44 +0800

    使用新特性 更新CourseDao CoursesService出错

--- Pro/src/main/java/com/ooad/demo/dao/CourseDao.java
+++ Pro/src/main/java/com/ooad/demo/dao/CourseDao.java
@@ -33,29 +33,49 @@ public class CourseDao {
     private TeamMapper teamMapper;
 
     /**
+     * 班级信息
+     */
+    public static final int HAS_CLASSES = 0;
+    /**
+     * 轮次信息
+     */
+    public static final int HAS_ROUNDS = 1;
+    /**
+     * 讨论课信息
+     */
+    public static final int HAS_SEMINARS = 2;
+    /**
+     * 教师信息
+     */
+    public static final int HAS_TEACHER = 3;
+
+
+    /**
      * Description:通过id获取Course，三个布尔值参数可以设置对应的查询表
      *
      * @Author:17Wang
      * @Time:14:44 2018/11/28
      */
-    public Course getById(int id, boolean hasClasses, boolean hasRounds, boolean hasSeminars, boolean hasTeacher) {
+    public Course getById(int id, int... hasSomething) {
         Course course = courseMapper.findById(id);
 
-        if (hasClasses) {
-            List<CClass> cClasses = cClassMapper.findByCourseId(id);
-            course.setcClasses(cClasses);
-        }
-        if (hasRounds) {
-            List<Round> rounds = roundMapper.findByCourseId(id);
-            course.setRounds(rounds);
-        }
-        if (hasSeminars) {
-            List<Seminar> seminars = seminarMapper.findByCourseId(id);
-            course.setSeminars(seminars);
-        }
-        if (hasTeacher) {
-            User teacher = userMapper.findById(course.getTeacherId());
-            course.setTeacher(teacher);
+        for (int i : hasSomething) {
+            if (i == HAS_CLASSES) {
+                List<CClass> cClasses = cClassMapper.findByCourseId(id);
+                course.setcClasses(cClasses);
+            }
+            if (i == HAS_ROUNDS) {
+                List<Round> rounds = roundMapper.findByCourseId(id);
+                course.setRounds(rounds);
+            }
+            if (i == HAS_SEMINARS) {
+                List<Seminar> seminars = seminarMapper.findByCourseId(id);
+                course.setSeminars(seminars);
+            }
+            if (i == HAS_TEACHER) {
+                User teacher = userMapper.findById(course.getTeacherId());
+                course.setTeacher(teacher);
+            }
         }
 
         return course;
@@ -67,26 +87,27 @@ public class CourseDao {
      * @Author:17Wang
      * @Time:23:23 2018/11/27
      */
-    public List<Course> listByTeacherId(int teacherId, boolean hasClasses, boolean hasRounds, boolean hasSeminars, boolean hasTeacher) {
+    public List<Course> listByTeacherId(int teacherId, int... hasSomething) {
         List<Course> courses = courseMapper.findByTeacherId(teacherId);
 
-        for (Course course :
-                courses) {
-            if (hasClasses) {
-                List<CClass> cClasses = cClassMapper.findByCourseId(course.getId());
-                course.setcClasses(cClasses);
-            }
-            if (hasRounds) {
-                List<Round> rounds = roundMapper.findByCourseId(course.getId());
-                course.setRounds(rounds);
-            }
-            if (hasSeminars) {
-                List<Seminar> seminars = seminarMapper.findByCourseId(course.getId());
-                course.setSeminars(seminars);
-            }
-            if (hasTeacher) {
-                User teacher = userMapper.findById(course.getTeacherId());
-                course.setTeacher(teacher);
+        for (Course course : courses) {
+            for (int i : hasSomething) {
+                if (i == HAS_CLASSES) {
+                    List<CClass> cClasses = cClassMapper.findByCourseId(course.getId());
+                    course.setcClasses(cClasses);
+                }
+                if (i == HAS_ROUNDS) {
+                    List<Round> rounds = roundMapper.findByCourseId(course.getId());
+                    course.setRounds(rounds);
+                }
+                if (i == HAS_SEMINARS) {
+                    List<Seminar> seminars = seminarMapper.findByCourseId(course.getId());
+                    course.setSeminars(seminars);
+                }
+                if (i == HAS_TEACHER) {
+                    User teacher = userMapper.findById(course.getTeacherId());
+                    course.setTeacher(teacher);
+                }
             }
         }
 
